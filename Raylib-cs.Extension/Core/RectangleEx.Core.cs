677201ebdb4eb1c322471fffee3df01f077dba3b
danilwhale <onefiler1@hotmail.com>
2023-10-25 18:23:14 +0300

    add method to create rectangle from vector

--- Raylib-cs.Extension/Core/RectangleEx.Core.cs
+++ Raylib-cs.Extension/Core/RectangleEx.Core.cs
@@ -1,6 +1,21 @@
+using System.Numerics;
+
 namespace Raylib_cs.Extension.Core;
 
 public static partial class RectangleEx
 {
-    
+    public static void FromVector(this ref Rectangle rectangle, Vector2 position, Vector2 size)
+    {
+        rectangle.x = position.X;
+        rectangle.y = position.Y;
+        rectangle.width = size.X;
+        rectangle.height = size.Y;
+    }
+
+    public static Rectangle FromVector(Vector2 position, Vector2 size)
+    {
+        Rectangle rectangle = new Rectangle();
+        rectangle.FromVector(position, size);
+        return rectangle;
+    }
 }
