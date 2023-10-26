ce7ed836977cb6d3933764143bd924f0294d18fd
danilwhale <onefiler1@hotmail.com>
2023-10-26 15:23:38 +0300

    comment rtextures module

--- Raylib-cs.Extensions/Textures/RenderTexture2DEx.Textures.cs
+++ Raylib-cs.Extensions/Textures/RenderTexture2DEx.Textures.cs
@@ -2,8 +2,16 @@ namespace Raylib_cs.Extensions;
 
 public static partial class RenderTexture2DEx
 {
+    /// <summary>
+    /// Check if a render texture is ready
+    /// </summary>
+    /// <param name="target"></param>
+    /// <returns></returns>
     public static bool IsReady(this RenderTexture2D target) => Raylib.IsRenderTextureReady(target);
+    
+    /// <summary>
+    /// Unload render texture from GPU memory (VRAM)
+    /// </summary>
+    /// <param name="target"></param>
     public static void Unload(this RenderTexture2D target) => Raylib.UnloadRenderTexture(target);
-    
-    
 }
