b1000e820eee0b5e54a85f189f93fc214b1bd8f2
danilwhale <onefiler1@hotmail.com>
2023-10-26 15:34:00 +0300

    comment rtext module

--- Raylib-cs.Extensions/Text/ColorEx.Text.cs
+++ Raylib-cs.Extensions/Text/ColorEx.Text.cs
@@ -4,18 +4,63 @@ namespace Raylib_cs.Extensions;
 
 public static partial class ColorEx
 {
+    /// <summary>
+    /// Draw text (using default font)
+    /// </summary>
+    /// <param name="tint"></param>
+    /// <param name="text"></param>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="fontSize"></param>
     public static void DrawText(this Color tint, string text, int x, int y, int fontSize)
         => Raylib.DrawText(text, x, y, fontSize, tint);
     
+    /// <summary>
+    /// Draw text using font and additional parameters
+    /// </summary>
+    /// <param name="tint"></param>
+    /// <param name="font"></param>
+    /// <param name="text"></param>
+    /// <param name="position"></param>
+    /// <param name="fontSize"></param>
+    /// <param name="spacing"></param>
     public static void DrawText(this Color tint, Font font, string text, Vector2 position, float fontSize, float spacing)
         => font.DrawText(text, position, fontSize, spacing, tint);
-
+    
+    /// <summary>
+    /// Draw text using Font and pro parameters (rotation)
+    /// </summary>
+    /// <param name="tint"></param>
+    /// <param name="font"></param>
+    /// <param name="text"></param>
+    /// <param name="position"></param>
+    /// <param name="origin"></param>
+    /// <param name="rotation"></param>
+    /// <param name="fontSize"></param>
+    /// <param name="spacing"></param>
     public static void DrawText(this Color tint, Font font, string text, Vector2 position, Vector2 origin, float rotation, float fontSize, float spacing)
         => font.DrawText(text, position, origin, rotation, fontSize, spacing, tint);
-
+    
+    /// <summary>
+    /// Draw one character (codepoint)
+    /// </summary>
+    /// <param name="tint"></param>
+    /// <param name="font"></param>
+    /// <param name="codepoint"></param>
+    /// <param name="position"></param>
+    /// <param name="fontSize"></param>
     public static void DrawCodepoint(this Color tint, Font font, char codepoint, Vector2 position, float fontSize)
         => font.DrawCodepoint(codepoint, position, fontSize, tint);
-
+    
+    /// <summary>
+    /// Draw multiple character (codepoint)
+    /// </summary>
+    /// <param name="tint"></param>
+    /// <param name="font"></param>
+    /// <param name="codepoints"></param>
+    /// <param name="position"></param>
+    /// <param name="fontSize"></param>
+    /// <param name="spacing"></param>
     public static void DrawCodepoints(this Color tint, Font font, char[] codepoints, Vector2 position, float fontSize, float spacing)
         => font.DrawCodepoints(codepoints, position, fontSize, spacing, tint);
 }
--- Raylib-cs.Extensions/Text/ImageEx.Text.cs
+++ Raylib-cs.Extensions/Text/ImageEx.Text.cs
@@ -2,5 +2,12 @@ namespace Raylib_cs.Extensions;
 
 public static partial class ImageEx
 {
+    /// <summary>
+    /// Load font from Image (XNA style)
+    /// </summary>
+    /// <param name="image"></param>
+    /// <param name="key"></param>
+    /// <param name="firstChar"></param>
+    /// <returns></returns>
     public static Font LoadFont(this Image image, Color key, char firstChar) => Raylib.LoadFontFromImage(image, key, firstChar);
 }
